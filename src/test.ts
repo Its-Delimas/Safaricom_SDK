00cfccfa628f4142a869f3e486d245dbf9c3d79b
Its-Delimas <spencerdelimas@gmail.com>
2026-07-31 12:56:55 +0300

    fix

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,8 @@
 import { MpesaClient } from "./client/MpesaClient";
 
 const mpesa = new MpesaClient ({
-    consumerKey:"cNQxmvoSJKpTXlGM6pSWWLmH3897OOB3exRwV6wuWuDyDdGU",
-    consumerSecret:"Gv61Tb8OckpfMD5hRN4R8j4uGT2P6O0w99X7K2MyxAg0uFtvaghKeuL0wLuOOzWf",
+    consumerKey:"",
+    consumerSecret:"",
     shortCode:"174379",
     passKey:"bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
     environment:"sandbox",
